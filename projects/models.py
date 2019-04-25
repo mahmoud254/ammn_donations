65b5d0731fb2ced8c3d81b60e32594db24f11b19
nuranlamei <lamei.nouran@gmail.com>
2019-04-25 15:21:04 +0200

    admin attepmt before model errors

--- projects/models.py
+++ projects/models.py
@@ -4,12 +4,15 @@ from User import models as User
 
 # Create your models here.
 
+class Category(models.Model):
+    categories = models.CharField(max_length=300)
+
 
 class Project(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     details = models.CharField(max_length=300)
-    category = models.CharField(max_length=100)
+    category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     total_target = models.IntegerField()
     start_date = models.DateField()
     end_date = models.DateField()
