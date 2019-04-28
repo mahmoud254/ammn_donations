22f4c91f11d04ccf3466b2ccf0ad93ece7a5e4f1
mahmoud254 <46284954+mahmoud254@users.noreply.github.com>
2019-04-28 15:29:16 +0200

    adding datepickers to create project form

--- ammn_donations/urls.py
+++ ammn_donations/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('projects/',include('projects.urls'))
+    path('projects/',include('projects.urls')),
+     path('jsi18n', JavaScriptCatalog.as_view(), name='javascript-catalog')
 ]
--- projects/forms.py
+++ projects/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from projects.models import Projects
 from projects.models import Category
-
+from django.contrib.admin import widgets
 class DocumentForm(forms.Form):
     title = forms.CharField(label='Title',required=True)
     tags = forms.CharField(label='enter tags separated by #',required=True)
     details =forms.CharField(widget=forms.Textarea,label='Details',required=True)
     total_target=forms.IntegerField(required=True)
-    start_date=forms.DateField(required=True)
-    end_date=forms.DateField(required=True)
+    start_date=forms.DateField(required=True,widget=widgets.AdminDateWidget)
+    end_date=forms.DateField(required=True,widget=widgets.AdminDateWidget)
     document=forms.FileField(required=True)
     result = [(q["id"], q['categories']) for q in Category.objects.values()]
     category_id = forms.ChoiceField(choices=result, required=True )
