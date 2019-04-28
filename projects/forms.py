fedd22358e3131e171abea51788cdc481b130336
mahmoud254 <46284954+mahmoud254@users.noreply.github.com>
2019-04-28 16:33:59 +0200

    enhancing css

--- projects/forms.py
+++ projects/forms.py
@@ -3,10 +3,10 @@ from projects.models import Projects
 from projects.models import Category
 from django.contrib.admin import widgets
 class DocumentForm(forms.Form):
-    title = forms.CharField(label='Title',required=True)
-    tags = forms.CharField(label='enter tags separated by #',required=True)
-    details =forms.CharField(widget=forms.Textarea,label='Details',required=True)
-    total_target=forms.IntegerField(required=True)
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter project title'}),required=True)
+    tags = forms.CharField( widget=forms.TextInput(attrs={'placeholder': 'enter tags separated by #'}),required=True)
+    details =forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'enter project details'}),label='Details',required=True)
+    total_target=forms.IntegerField(required=True,widget=forms.TextInput(attrs={'placeholder': 'enter total target in dollars'}))
     start_date=forms.DateField(required=True,widget=widgets.AdminDateWidget)
     end_date=forms.DateField(required=True,widget=widgets.AdminDateWidget)
     document=forms.FileField(required=True)
