a1d07668b89b3c73cbb5cb5a072cd058988fe3a7
Mahmoud Alaa <46284954+mahmoud254@users.noreply.github.com>
2020-04-25 15:48:53 +0200

    Update settings.py

--- ammn_donations/settings.py
+++ ammn_donations/settings.py
@@ -83,18 +83,12 @@ WSGI_APPLICATION = 'ammn_donations.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ammn',
-        'USER': 'pythonuser',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
